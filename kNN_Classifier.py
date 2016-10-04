ea464d155297c8e72de2b3a5ef5f487f3d8be667
Daniel Sanche <d.sanche14@gmail.com>
2016-10-03 19:34:51 -0600

    Changed implementation to use loops, because it's necessary to update the test data as we go

--- kNN_Classifier.py
+++ kNN_Classifier.py
@@ -22,32 +22,39 @@ def plotData(pandasData, pdf_name="plot.pdf", display_figure=True):
 
 def kNN(trainData, testData, k=1, feedback_classification=False):
     pointArr = trainData.as_matrix(columns=trainData.columns[0:2])
-    pointArr =  np.expand_dims(pointArr, 2)
-    labelArr = trainData.as_matrix(columns=trainData.columns[2:])
+    labelArr = trainData.as_matrix(columns=trainData.columns[2:]).astype(np.uint8)
 
     testArr = testData.as_matrix()
-    testArr = np.expand_dims(testArr, 2)
-    testArr = np.transpose(testArr, (2,1,0))
+    results = np.zeros(testArr.shape[0])
 
-    #find the difference between each point in the test set, and each point in the training set
-    diffArr = pointArr - testArr
-    #square each cell
-    diffArr = np.square(diffArr)
-    #add the difference in x and y value together
-    #(no need to square this value, because we are looking at relative distances)
-    diffArr = np.sum(diffArr, axis=1)
-    #combine the diffArr and labelArr, so we can sort and keep the labels
-    combined = np.dstack((diffArr, np.matlib.repmat(labelArr, 1, diffArr.shape[1])))
-    #sort the array by the 1st dimension to find the smallest distances
-    sorted = np.sort(combined, 1)
-    #trim the matrix to only keep the top k neighbours
-    sorted = sorted[0:k,:,1]
-    #find the median of each row to determine the top class for each point
-    result = np.median(sorted, axis=0)
-    return result
+    for i in range(0, testArr.shape[0]):
+        #find the next point in the training set
+        thisPt = testArr[i,:]
+        # find the difference between each point in the test set and the point we are classifying
+        distanceMat = pointArr - thisPt
+        #square the x and y differences
+        distanceMat = np.square(distanceMat)
+        # add the difference in x and y value together
+        # (no need to square this value, because we are looking at relative distances)
+        distanceMat = np.sum(distanceMat, axis=1)
+        #find the indices of the sorted list, so that we know which points are closest
+        sort_indices = np.argsort(distanceMat)
+        #sort the label array using these indices to find the top labels
+        closestLabels = labelArr[sort_indices]
+        closestLabels = closestLabels[0:k]
+        #take the median of the top k results to find the class
+        topResult = np.median(closestLabels)
+        #store it in the array of other results
+        results[i] = topResult
+        if feedback_classification:
+            pointArr = np.append(pointArr, thisPt.reshape(1,2), axis=0)
+            labelArr = np.append(labelArr, topResult.reshape(1,1), axis=0)
+    return results
+
 
 data = pd.read_csv("knnDataSet.csv")
 trainData = data[data["L"].notnull()]
 testData = data[data["L"].isnull()]
 
-kNN(trainData[['x', 'y', 'L']], testData[['x', 'y']], k=5)
+newLabels = kNN(trainData[['x', 'y', 'L']], testData[['x', 'y']], k=5, feedback_classification=True)
+print (newLabels)
