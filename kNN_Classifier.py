1c6f1de81a75f9bca51d4c3728ee2c8aceadacb2
Daniel Sanche <d.sanche14@gmail.com>
2016-10-03 20:17:43 -0600

    Added function to compute and display confusion matrix

--- kNN_Classifier.py
+++ kNN_Classifier.py
@@ -5,13 +5,13 @@ import pandas as pd
 import numpy as np
 import numpy.matlib
 
-def plotData(trainData, testData, classifiedTestLabels=None, pdf_name="plot.pdf", display_figure=True):
+def plotData(trainData, testData, classifiedLabels=None, pdf_name="plot.pdf", display_figure=True):
     colors = ["r", "g", "b", "m", "y"]
     fig = plt.figure()
     for i in range(1, 6):
         filteredTrain = trainData[trainData["TL"]==i]
-        if classifiedTestLabels is not None:
-            filteredTest = testData[classifiedTestLabels == i]
+        if classifiedLabels is not None:
+            filteredTest = testData[classifiedLabels == i]
         else:
             filteredTest = testData[testData["TL"] == i]
         plt.scatter(filteredTest["x"], filteredTest["y"], c=colors[i - 1], marker="x")
@@ -21,6 +21,31 @@ def plotData(trainData, testData, classifiedTestLabels=None, pdf_name="plot.pdf"
     pp = PdfPages(pdf_name)
     pp.savefig(fig)
     pp.close()
+
+def printConfusionMatrix(labeledClasses, groundTruth, titleStr="Confusion Matrix:"):
+    labeledClasses = labeledClasses.astype(np.uint8)
+    groundTruth = groundTruth.astype(np.uint8)
+    num_classes = np.amax(groundTruth)
+    confusionMat = np.zeros([num_classes, num_classes], dtype=int)
+    totalNum = 0
+    totalCorrect = 0
+
+    for i in range(0, labeledClasses.shape[0]):
+        truth = groundTruth[i,0].astype(np.uint8)
+        guess = labeledClasses[i].astype(np.uint8)
+        confusionMat[truth-1, guess-1] += 1
+        totalNum += 1
+        if truth == guess:
+            totalCorrect += 1
+    print(titleStr, end="\n\t\t")
+    for i in range(0,num_classes):
+        print(i, end="\t\t")
+    for i in range(0,num_classes):
+        print("\n"+str(i)+"\t\t", end="")
+        for j in range(0,num_classes):
+            print(confusionMat[i,j], end="\t\t")
+    print ("\n"+str(totalCorrect)+"/"+str(totalNum)+" = "+"%.2f" % round((totalCorrect/totalNum*100),2)+"%")
+
 
 def kNN(trainData, testData, k=1, feedback_classification=False):
     pointArr = trainData.as_matrix(columns=trainData.columns[0:2])
@@ -59,5 +84,5 @@ trainData = data[data["L"].notnull()]
 testData = data[data["L"].isnull()]
 plotData(trainData, testData, display_figure=False, pdf_name="GroundTruth.pdf")
 newLabels = kNN(trainData[['x', 'y', 'L']], testData[['x', 'y']], k=1, feedback_classification=False)
-plotData(trainData, testData, classifiedTestLabels=newLabels, pdf_name="K1.pdf", display_figure=False)
-print (newLabels)
+printConfusionMatrix(newLabels, testData[['TL']].as_matrix())
+plotData(trainData, testData, classifiedLabels=newLabels, pdf_name="K1.pdf", display_figure=False)
